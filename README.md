ce406c761ff4d300bd87ab09257181826af97d92
srilatha2012 <srilathp2012@gmail.com>
2026-04-06 00:30:47 -0400

    Added Product api simulator with promise chaining and modified README.md file

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 ## Product Api Simulator 
+This project simulate a small Product API system typescript and promises.
 
+## Features
+. Fetch a product catelog asynchronously
+. Fetches Reviews based on product id
+. Fetches Sales report after product and review data
+
+## Technologies used
+. Typescript
+. Promises
+. Custom Error classes
+. Mock API simulation with setTimeOut
+
+## Project Structure
+. apiSimulator.ts
+. main.ts
+
+## Custom Error clasess
+- NetworkError
+- DataError
+
+## How the application works
+1. The application first fetches the product catalog info
+2. After fetching products, it fetches reviews based on product id
+3. Once all products and reviews fetched it fetches sales report
+4. if any call fails, the error is handled in the .catch() block
+5. The .finally() block logs that all API calls have been attempted
+
+## Challenges faced
+One Challenge was understanding how to chain muliple Promise based functions in the correct order
+
+
+## Reflection
 1. Why is it important to handle errors for each individual API call rather than just at the end of the promise chain?
+Handling errors makes it easier to understand where exactly the failure happened. 
+
 2. How does using custom error classes improve debugging and error identification?
+Custom error classes make errors more meaningfull and easier to identify
+
 3. When might a retry mechanism be more effective than an immediate failure response?
+A retry mechanism is more effective when the failure is temporary rather than permanent
--- src/main.ts
+++ src/main.ts
@@ -6,58 +6,59 @@ import {
     DataError
 } from "./apiSimulator.js"
 
-function handleApiCalls() :void {
-     fetchProductCatalog()
-     .then((products)=>{
-        console.log("Product Catelog");
-        console.log(products);
-     })
-     .then(()=>{
+function handleApiCalls(): void {
 
-     })
-     .catch((error) =>{
-        if(error instanceof NetworkError) {
-            console.error("Network error:" ,error.message);
-        } else if(error instanceof DataError) {
-            console.error("Data Error:",error.message);
-        } else if( error instanceof Error) {
-            console.error("Error:",error.message);
-        } else {
-            console.error("Unknown Error:", error);
-        }
-     })
-     .finally(()=> {
-        console.log("All API calls have been attempted.");
-     })
+    //Step:1 fetch product catalog
+    fetchProductCatalog()
+        .then((products) => {
+            console.log("Product Catelog");
+            console.log(products);
+
+            // Step2: for each product, fetch it reviews 
+            const reviewPromises = products.map((product) => {
+                return fetchProductReviews(product.id).then((reviews) => {
+                    return {
+                        product,
+                        reviews
+                    };
+                });
+            });
+            //wait until all review api calls are completed
+            return Promise.all(reviewPromises);
+
+        })
+        .then((productReviewData) => {
+            console.log("product reviews");
+
+            //Display reviews based on product id
+            productReviewData.forEach((item) => {
+                console.log(`Reviews for Item ${item.product.name}`);
+                console.log(item.reviews);
+            });
+
+            //Step 3: fetch the sales report after product and reviews    
+            return fetchSalesReport();
+        })
+        .then((salesReport) => {
+            console.log("Sales Report");
+            console.log(salesReport);
+        })
+        //Step 4: handle errors from any API call
+        .catch((error) => {
+            if (error instanceof NetworkError) {
+                console.error("Network error:", error.message);
+            } else if (error instanceof DataError) {
+                console.error("Data Error:", error.message);
+            } else if (error instanceof Error) {
+                console.error("Error:", error.message);
+            } else {
+                console.error("Unknown Error:", error);
+            }
+        })
+
+        //Step 5: Whether the api calls are succeeed or fail
+        .finally(() => {
+            console.log("All API calls have been attempted.");
+        })
 }
-// try {
-//     fetchProductCatalog()
-//         .then((products) => {
-//             console.log(products);
-//             const productId = products[0]?.id;
-//             if (productId === undefined) {
-//                 throw new Error("Product Id not found");
-//             }
-//             return fetchProductReviews(productId);
-
-//         })
-//         .then((reviews) => {
-//             console.log(reviews);
-//             if (reviews === undefined) {
-//                 throw new Error("Reviews not found");
-//             }
-//             return fetchSalesReport();
-
-//         })
-//         .then((salesReport) => {
-//             console.log(salesReport);
-//         })
-//         .catch((error) => {
-//             console.error("An error occured", error.message);
-//         });
-
-// } catch (error) {
-
-
-
 handleApiCalls();
